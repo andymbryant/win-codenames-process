80991ea8e3c34d945f27f616c2f0ea90c6555ceb
Andy Bryant <a.mcdermid.bryant@gmail.com>
2020-08-01 21:18:19 -0400

    streamlined deep processing, added outputs

--- helpers.py
+++ helpers.py
@@ -6,8 +6,12 @@ from kneed import KneeLocator
 # import nbconvert.filters.strings
 from scipy.spatial.distance import squareform
 from scipy.spatial.distance import pdist
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster, leaves_list, ward, optimal_leaf_ordering
+from sklearn.cluster import AgglomerativeClustering
+from matplotlib import pyplot as plt
+from statistics import mean
 
-CONSTANT = 4
+# CONSTANT = 6
 
 def get_game_words(words_for_game_df):
     # Get sample from dataframe
@@ -34,29 +38,26 @@ def distance(source, target, vectors):
     return spatial.distance.cosine(vectors.loc[source].to_numpy(), vectors.loc[target].to_numpy())
 
 def get_top_friends(vectors, friends):
-    # Get optimal friends
     data = np.array([vectors.loc[word].to_numpy() for word in friends])
+    cos_data = pdist(data, metric='cosine')
     pairwise_cos = pd.DataFrame(
-        squareform(pdist(data, metric='cosine')),
+        squareform(cos_data),
         columns = friends,
         index = friends
     ).replace(to_replace=0, value=999)
-    data_dict = dict(pairwise_cos.min())
-    y = list(data_dict.values())
-    x = range(1, len(data_dict.keys())+1)
-    kn = KneeLocator(x, y, curve='convex', direction='decreasing')
-    if kn.knee > 1:
-        num_friends_to_consider = kn.knee
-    else:
-        # Randomly make it 4 sometimes?
-        num_friends_to_consider = 2
-    top_friends = sorted(data_dict, key=data_dict.get, reverse=False)[:num_friends_to_consider]
-    print(f'Friends: {friends}')
-    print(f'Top friends: {top_friends}')
+    # How much should this be? More will favor larger clusters.
+    MULT = 1.2
+    avg_dist = mean(pairwise_cos.min()) * MULT
+    Z = linkage(cos_data, method='complete', optimal_ordering=True)
+    cos_indices = fcluster(Z, t=avg_dist, criterion='distance')
+    np_friends = np.array(cos_indices)
+    indices = np.where(np_friends == 1)[0]
+    top_friends = [friends[i] for i in indices]
+
     low_friends = set(friends) - set(top_friends)
     return top_friends, low_friends
 
-def get_scores(row, vectors, **kwargs):
+def get_scores(row, vectors, primary, **kwargs):
     word = row.word
     friends = kwargs.get('friends')
     board_words = kwargs.get('board_words')
@@ -65,25 +66,39 @@ def get_scores(row, vectors, **kwargs):
     low_friends = kwargs.get('low_friends')
     foes = kwargs.get('foes')
     neutrals = kwargs.get('neutrals')
-    if word in board_words or any([bw in word for bw in board_words]):
-        goodness = assassin_minimax = foes_minimax = neutrals_minimax = variance = -1000
-    else:
-        assassin_dist = [distance(word, a, vectors) for a in assassin]
-        # Check if assassin distance is adequate, if not don't waste your time
-        if abs(assassin_dist[0]) > 0.001:
-            top_friends_dist = [distance(word, tf, vectors) for tf in top_friends]
-            all_friends_dist = [distance(word, lf, vectors) for lf in low_friends]
-            foes_dist = [distance(word, f, vectors) for f in foes]
-            neutrals_dist = [distance(word, n, vectors) for n in neutrals]
-            goodness = sum(foes_dist + assassin_dist) - CONSTANT * sum(top_friends_dist)
-            min_friends_dist = min(all_friends_dist)
-            max_friends_dist = max(all_friends_dist)
-            assassin_minimax = min(assassin_dist) - max_friends_dist
-            foes_minimax = min(foes_dist) - max_friends_dist
-            neutrals_minimax = min(neutrals_dist) - max_friends_dist
-            # Should this be absolute value?
-            variance = abs(max_friends_dist) - abs(min_friends_dist)
-    return pd.Series([goodness, assassin_minimax, foes_minimax, neutrals_minimax, variance])
+
+    # Initialize with na values
+    goodness = assassin_dist = bad_minimax = neutrals_minimax = variance = np.nan
+    na_series = pd.Series([goodness, bad_minimax, neutrals_minimax, variance])
+
+    if word.lower() in board_words or any([bw in word.lower() for bw in board_words]):
+        return na_series
+
+    # If first candidates_df (glove) then check for assassin distance
+    # If not, the assassin distance will be very different for the other models
+    assassin_dist = [distance(word, a, vectors) for a in assassin]
+    # Check if assassin distance is adequate, if not don't waste your time
+    # Acceptable ~.8
+    if primary and assassin_dist[0] <= 0.8:
+        return na_series
+
+    top_friends_dist = [distance(word, tf, vectors) for tf in top_friends]
+    # changed this to to all friends, not low friends
+    low_friends_dist = [distance(word, lf, vectors) for lf in low_friends]
+    foes_dist = [distance(word, f, vectors) for f in foes]
+    neutrals_dist = [distance(word, n, vectors) for n in neutrals]
+    bad_dist = foes_dist + assassin_dist
+    goodness = (sum(bad_dist)/len(bad_dist)) - (sum(top_friends_dist)/len(top_friends_dist))
+    # OLD WAY OF CALCULATING GOODNESS
+    # goodness = sum(foes_dist + assassin_dist) - CONSTANT * sum(top_friends_dist)
+    friends_dist = top_friends_dist + low_friends_dist
+    min_friends_dist = min(friends_dist)
+    max_friends_dist = max(friends_dist)
+    bad_minimax = min(bad_dist) - max_friends_dist
+    neutrals_minimax = min(neutrals_dist) - max_friends_dist
+    # Should this be absolute value?
+    variance = abs(max_friends_dist) - abs(min_friends_dist)
+    return pd.Series([goodness, bad_minimax, neutrals_minimax, variance])
 
 def get_final_metrics(word, candidates, **kwargs):
     total_rank = 0
@@ -91,17 +106,25 @@ def get_final_metrics(word, candidates, **kwargs):
     total_goodness = 0
     for candidate_df in candidates:
         word_select = candidate_df['word'] == word
-        rank = candidate_df.index[word_select].tolist()[0]
+        rank = candidate_df[word_select].index[0]
         total_rank = total_rank + rank
-        variance = candidate_df[word_select].variance.tolist()[0]
+        variance = candidate_df[word_select].variance.iloc[0]
         total_variance = total_variance + variance
-        goodness = candidate_df[word_select].goodness.tolist()[0]
+        goodness = candidate_df[word_select].goodness.iloc[0]
         total_goodness = total_goodness + goodness
+        # Multiply by 1.5 to favor the the high and penalize the low rank (lower number is higher rank)
     return pd.Series([total_rank * 1.5, (total_variance/len(candidates)), (total_goodness/len(candidates))])
 
-def get_candidates_df(vectors, **kwargs):
-    all_words = kwargs.get('all_words')
-    candidates = pd.DataFrame({'word': all_words, 'frequency': [i for i in range(1, len(all_words) + 1)]})
-    candidates[['goodness', 'assassin_minimax', 'foes_minimax', 'neutrals_minimax', 'variance']] = candidates.apply(lambda row: get_scores(row, vectors, **kwargs), axis=1)
-    sort_by_columns = ['goodness', 'foes_minimax', 'assassin_minimax', 'frequency', 'neutrals_minimax', 'variance']
-    return candidates.sort_values(sort_by_columns, ascending=[False for i in range(len(sort_by_columns))]).reset_index(drop=True)
+def get_candidates_df(vectors, primary, **kwargs):
+    words_to_consider = kwargs.get('words_to_consider')
+    words_to_consider_frequencies = kwargs.get('words_to_consider_frequencies')
+    print(words_to_consider[:25])
+    print(words_to_consider_frequencies[:25])
+    candidates = pd.DataFrame({'word': words_to_consider, 'frequency': words_to_consider_frequencies})
+    candidates[['goodness', 'bad_minimax', 'neutrals_minimax', 'variance']] = candidates.apply(lambda row: get_scores(row, vectors, primary, **kwargs), axis=1)
+    candidates.dropna(inplace=True)
+    sort_by_columns = ['goodness', 'bad_minimax', 'frequency', 'neutrals_minimax']
+    candidates = candidates.sort_values(sort_by_columns, ascending=[False for i in range(len(sort_by_columns))]).reset_index(drop=True)
+    print('candidates after sort:')
+    print(candidates)
+    return candidates
--- run.py
+++ run.py
@@ -4,9 +4,12 @@ from helpers import *
 
 def main():
 
-    express = False
-    size = 25
-    num_iterations = 15
+    # Make express even more express
+    # Increase threshold of assassin for passing
+    # I should probably change it from assassin as the threshold - high friends?
+    # Also no need to combine the DataFrame
+    deep = True
+    num_iterations = 1
 
     # Import Glove vectors
     glove_vectors = pd.read_pickle('./processing/data/glove_vectors.pkl')
@@ -17,7 +20,8 @@ def main():
     all_words = list(glove_vectors.index.get_level_values(level=0))
 
     for i in range(num_iterations):
-        # Import words for game and make categorized lists from them
+        print(f'Iteration: {i + 1}')
+        # Import words for game and make categorized lists from them (i.e. generate cards for game)
         words_for_game_df = pd.read_json('./processing/data/words_v2.json')
         friends, foes, neutrals, assassin, board_words = get_game_words(words_for_game_df)
         top_friends, low_friends = get_top_friends(glove_vectors, friends)
@@ -31,27 +35,40 @@ def main():
             'top_friends': top_friends,
             'low_friends': low_friends,
             'board_words': board_words,
-            'all_words': all_words
+            'words_to_consider': all_words,
+            'words_to_consider_frequencies': [i for i in range(1, len(all_words) + 1)]
         }
 
-        if express:
-            glove_candidates = get_candidates_df(glove_vectors, **words_dict)
-            top_candidate_words = glove_candidates.word.tolist()[:size]
-            all_candidates = [glove_candidates]
-        else:
+        size = 2000 if deep else 250
+        print('Start glove candidates')
+        glove_candidates = get_candidates_df(glove_vectors, True, **words_dict)
+        top_candidate_words = glove_candidates.word.tolist()[:size]
+        all_candidates = [glove_candidates]
+        if deep:
             google_vectors = pd.read_pickle('./processing/data/google_vectors.pkl')
             ft_vectors = pd.read_pickle('./processing/data/fasttext_vectors.pkl')
-            glove_candidates = get_candidates_df(glove_vectors, **words_dict)
-            google_candidates = get_candidates_df(google_vectors, **words_dict)
-            ft_candidates = get_candidates_df(ft_vectors, **words_dict)
-            top_candidate_words = glove_candidates.word.tolist()[:size] + google_candidates.word.tolist()[:size] + ft_candidates.word.tolist()[:size]
+            # print('Printing top glove candidates.')
+            words_dict['words_to_consider'] = top_candidate_words
+            # Get word frequencies from glove
+            words_dict['words_to_consider_frequencies'] = [glove_candidates[glove_candidates.word == word].frequency.iloc[0] for word in top_candidate_words]
+            print('Start google candidates')
+            google_candidates = get_candidates_df(google_vectors, False, **words_dict)
+            print('Start ft candidates')
+            ft_candidates = get_candidates_df(ft_vectors, False, **words_dict)
+            top_glove_words = set(glove_candidates.word.tolist())
+            top_google_words = set(google_candidates.word.tolist())
+            top_ft_words = set(ft_candidates.word.tolist())
+            # Only consider words that are in all three outputs
+            top_candidate_words = list(top_glove_words.intersection(top_google_words, top_ft_words))
             all_candidates = [glove_candidates, google_candidates, ft_candidates]
 
         final_candidates = pd.DataFrame({'word': top_candidate_words})
+        print('starting get final metrics')
         final_candidates[['rank', 'variance', 'goodness']] = final_candidates.word.apply(lambda word: get_final_metrics(word, all_candidates))
-        final_candidates = final_candidates.drop_duplicates(subset=['word'])
+        # final_candidates = final_candidates.drop_duplicates(subset=['word'])
         final_candidates.sort_values(['rank', 'variance', 'goodness'], ascending=[True,True,False]).reset_index(drop=True)
-
+        print('final candidates:')
+        print(final_candidates)
         new_row = [
             " ".join(list(final_candidates.word)[:10]),
             " ".join(top_friends),
